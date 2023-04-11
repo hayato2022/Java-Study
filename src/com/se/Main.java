4cf3ae951a5870d1bd380ce66ca2eb6ed206d74f
hayato0314 <gaomusunxiang@gmail.com>
2023-04-11 16:31:00 +0900

    コメント追加

--- src/com/se/Main.java
+++ src/com/se/Main.java
@@ -7,12 +7,12 @@ public class Main {
 		int i1 = 100;
 		Integer obj = i1; // Boxing
 		int i2 = obj; // Unboxing
-		method(i2);
+		method(i2); // int型でメソッドの引数に渡す
 		
  	}
-	static void method(Integer obj) {
-		int i = obj + 100;
-		System.out.println(i);
+	static void method(Integer obj) {  // Integer型で受け取る
+		int i = obj + 100;   // 計算も可能
+		System.out.println(i); 
 	}
 
 }
