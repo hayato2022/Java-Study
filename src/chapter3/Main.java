f93059d32fc63928e4c3b54e2a04004601b282ce
hayato0314 <gaomusunxiang@gmail.com>
2023-04-13 17:08:50 +0900

    while文とdo-while文の違い

--- src/chapter3/Main.java
+++ src/chapter3/Main.java
@@ -11,6 +11,22 @@ public class Main {
 		
 		DoWhilePractise dw = new DoWhilePractise();
 		dw.doWhile();
+		System.out.println("  ");
+		
+		// while文とdo-while文の違い
+		// while文の場合
+		int count = 5;
+		while(count != 5 && count > 0) {
+			System.out.println("while  : count = " + count);
+			count--;
+		}
+		
+		// do-while文の場合
+		count = 5;
+		do {
+			System.out.println("do-while  : count = " + count);
+			count--;
+		} while (count != 5 && count > 0);
 	}
 
 }
