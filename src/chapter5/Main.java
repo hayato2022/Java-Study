860f0f6c2d3d61fe7f678c170188063746f7ec70
hayato0314 <gaomusunxiang@gmail.com>
2023-04-19 21:29:14 +0900

    static学習

--- src/chapter5/Main.java
+++ src/chapter5/Main.java
@@ -10,18 +10,39 @@ public class Main {
 //		System.out.println("IDは" + cp.getId());
 		
 		// メソッドのオーバーロード
-		Test t = new Test();
-		t.myPrint();
-		t.myPrint(8810);
-		t.myPrint("test");
-		t.myPrint(300, 400);
+//		Test t = new Test();
+//		t.myPrint();
+//		t.myPrint(8810);
+//		t.myPrint("test");
+//		t.myPrint(300, 400);
+		
+		// 可変長引数
+//		Test obj1 = new Test();
+//		int[] array = {10, 20, 30};
+//		obj1.method("1回目");
+//		obj1.method("2回目", 10);
+//		obj1.method("3回目", 10, 20, 30, 40);
+//		obj1.method("4回目", array);
+		
+		// インスタンスメンバとstaticメンバの違い
+//		System.out.println(Test.instanceVal); NG
+		System.out.println(Test.staticVal); //OK
+//		Test.methodA();  NG
+		Test.methodB();  //OK
+		Test obj2 = new Test();
+		System.out.println(obj2.instanceVal);  //OK
+		obj2.methodA();  //OK
+		obj2.methodB();  //OK
 		
 	}
 	
-	Employee e = new Employee("aaa");
-	Employee e1 = new Employee("aaaa", 100);
-	Employee e2 = new Employee();
-	Employee e3 = new Employee(200);
+		
 	
+	
+	
+//	Employee e = new Employee("aaa");
+//	Employee e1 = new Employee("aaaa", 100);
+//	Employee e2 = new Employee();
+//	Employee e3 = new Employee(200);
 
 }
--- src/chapter5/Test.java
+++ src/chapter5/Test.java
@@ -1,6 +1,40 @@
 package chapter5;
 
 public class Test {
+	// インスタンスメンバとstaticメンバの違い
+	int instanceVal = 100;
+	static int staticVal = 200;
+	void methodA() {
+		System.out.println("methodA(): " + instanceVal);
+	}
+	
+	static void methodB() {
+		System.out.println("methodB(): " + staticVal);
+	}
+	
+	// staticメンバのクラス内でのアクセス
+	int methodC() {
+		return instanceVal;  // インスタンスメソッド→インスタンス変数のためOK
+	}
+	
+	int methodD() {
+		return staticVal;  // インスタンスメソッド→static変数のため OK
+	}
+	
+//	static methodE() {
+//		return instanceVal;  staticメソッド→インスタンス変数のためNG
+//	}
+	
+	static int methodE() {
+		return staticVal;  // staticメソッド→static変数のためOK
+	}
+	
+	static int methodF() {
+		Test obj = new Test();		// staticメソッド内で自クラスをインスタンス化し、
+		return obj.instanceVal; 	//  変数obj.インスタンス変数でアクセスしているためOK
+	}
+	
+	
 	// メソッドのオーバーロード
 	public void myPrint() {
 		System.out.println("myPrint()");
@@ -17,4 +51,17 @@ public class Test {
 	public void myPrint(int a, int b) {
 		System.out.println("myPrint(int a, int b): " + a + " " + b);
 	}
+	
+	
+	// 可変長引数
+	public void method(String s, int... a) {
+		System.out.println(s + "サイズ" + a.length);
+		for(int i : a) {
+			System.out.println(" 第2引数の値 :" + i);
+		}
+	}
+	
+	
+	
+	
 }
